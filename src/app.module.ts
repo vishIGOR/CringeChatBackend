ea67b6ffb7909c922e563242ba35c7a12f500b5b
DESKTOP-3QKRIRT\асус <vish_ig2@mail.ru>
2022-06-07 20:17:22 +0700

    Installed npm pockets for project. Deleted useless files from init nest template. Added db module and .env file with environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { AuthModule } from "./auth/auth.module";
+import { DatabaseConfigModule } from "./database/database.module";
+import { DatabaseConfigService } from "./database/database.config";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      inject: [DatabaseConfigService],
+      useFactory: async (configService: DatabaseConfigService) => configService.getMongoConfig()
+    }),
+    DatabaseConfigModule,
+    AuthModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
-export class AppModule {}
+export class AppModule {
+}
